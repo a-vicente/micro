fdab2aa10da0e0b42f09ca639ac7af42dcf5bcd7
Arnaldo Vicente <arnaldorjvicente@gmail.com>
2020-05-20 03:08:50 +0200

    Adicao do email de Acesso para Homo e alteracao do Dominio do email de efinderi para micro

--- src/main/resources/db/migration/V06__inserir_usuario_administrador.sql
+++ src/main/resources/db/migration/V06__inserir_usuario_administrador.sql
@@ -1,1 +1,4 @@
-INSERT INTO usuario (nome, email, senha, ativo) VALUES ('Arnaldo Vicente', 'arnaldo.vicente@efinderi.com', '$2a$10$g.wT4R0Wnfel1jc/k84OXuwZE02BlACSLfWy6TycGPvvEKvIm86SG', 1)
+INSERT INTO usuario (nome, email, senha, ativo) VALUES ('Arnaldo Vicente', 'arnaldo.vicente@micro.com', '$2a$10$g.wT4R0Wnfel1jc/k84OXuwZE02BlACSLfWy6TycGPvvEKvIm86SG', 1);
+
+INSERT INTO usuario (nome, email, senha, ativo) VALUES ('Nelson Homo', 'nelson.homo@micro.com', '$2a$10$g.wT4R0Wnfel1jc/k84OXuwZE02BlACSLfWy6TycGPvvEKvIm86SG', 1)
+
--- src/main/resources/db/migration/V07__inserir_permissoes_e_relacionar_usuario_admin.sql
+++ src/main/resources/db/migration/V07__inserir_permissoes_e_relacionar_usuario_admin.sql
@@ -1,7 +1,7 @@
 INSERT INTO permissao VALUES (1, 'ROLE_ADMINISTRADOR');
 INSERT INTO permissao VALUES (2, 'ROLE_APF');
-INSERT INTO permissao VALUES (3, 'ROLE_CALL_CENTER');
-INSERT INTO permissao VALUES (4, 'ROLE_MARKETING');
+INSERT INTO permissao VALUES (3, 'ROLE_CLIENT_MANAGER');
+INSERT INTO permissao VALUES (4, 'ROLE_ACCOUNTANT');
 
 INSERT INTO grupo_permissao (codigo_grupo, codigo_permissao) VALUES (1, 1);
 INSERT INTO grupo_permissao (codigo_grupo, codigo_permissao) VALUES (1, 2);
@@ -9,4 +9,7 @@ INSERT INTO grupo_permissao (codigo_grupo, codigo_permissao) VALUES (1, 3);
 INSERT INTO grupo_permissao (codigo_grupo, codigo_permissao) VALUES (1, 4);
 
 INSERT INTO usuario_grupo (codigo_usuario, codigo_grupo) VALUES (
-	(SELECT codigo FROM usuario WHERE email = 'arnaldo.vicente@efinderi.com'), 1);
+	(SELECT codigo FROM usuario WHERE email = 'arnaldo.vicente@micro.com'), 1);
+	
+INSERT INTO usuario_grupo (codigo_usuario, codigo_grupo) VALUES (
+	(SELECT codigo FROM usuario WHERE email = 'nelson.homo@micro.com'), 1);
